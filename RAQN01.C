cc2483064e5013f158af5e59061581e319cc2907
RahailKhan110 <143491504+RahailKhan110@users.noreply.github.com>
2023-08-29 13:11:23 +0500

    Add files via upload

---
+++ RAQN01.C
@@ -0,0 +1,11 @@
+
+#include<stdio.h>
+#include<conio.h>
+int main ()
+{
+clrscr();
+	printf("Name: RAHAIL KHAN\n Father's Name: RAZA MUHAMMAD\n Age : 18 Years \nGrade:  A1");
+	getch();
+	return 0;
+
+}
